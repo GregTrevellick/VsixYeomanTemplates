e2817b0659fa4f13b97ed8598458df03aa345db0
Greg Trevellick <gtrevellick@googlemail.com>
2018-12-29 08:23:41 +0000

    .

--- Src/AngularBasicVsix/UserInputForm.cs
+++ Src/AngularBasicVsix/UserInputForm.cs
@@ -7,71 +7,71 @@ namespace AngularBasicVsix
 {
     public partial class UserInputForm : Form 
     {
-        //////////////////////////////////////////////private Label label1;
-        //////////////////////////////////////////////private Button btnOk;
-        //////////////////////////////////////////////private Button btnCancel;
-
-        ////////////////////////////////////////////private void btnOk_Click(object sender, EventArgs e)
-        ////////////////////////////////////////////{
-        ////////////////////////////////////////////    Close();
-        ////////////////////////////////////////////}
-
-        ////////////////////////////////////////////private void btnCancel_Click(object sender, EventArgs e)
-        ////////////////////////////////////////////{
-        ////////////////////////////////////////////    Close();
-        ////////////////////////////////////////////}
-
         public UserInputForm(string solutionDirectory, string tempDirectory, string generatorName, string regularProjectName)
         {
             SuspendLayout();
 
-            #region Add form controls
-            #region Form text
-            var label1 = new Label();
-            label1.AutoSize = true;
-            label1.Location = new Point(25, 22);
-            label1.Name = "label1";
-            //label1.Size = new Size(35, 13);
-            //label1.TabIndex = 0;
-            label1.Text = DialogHelper.GetLabelText(solutionDirectory, tempDirectory, generatorName, regularProjectName);
-            Controls.Add(label1);
-            #endregion
+            AddFormControls(solutionDirectory, tempDirectory, generatorName, regularProjectName);
 
-            #region Okay button
-            var btnOk = new Button();
-            btnOk.DialogResult = DialogResult.OK;
-            btnOk.Location = new Point(464, 202);
-            btnOk.Name = "btnOk";
-            btnOk.Size = new Size(75, 23);
-            btnOk.TabIndex = 0;/////////////////// 1;
-            btnOk.Text = "OK";
-            btnOk.UseVisualStyleBackColor = true;
-            btnOk.Click += new EventHandler(btnOkCancel_Click);
-            Controls.Add(btnOk);
-            #endregion
-
-            #region Cancel button
-            var btnCancel = new Button();
-            btnCancel.DialogResult = DialogResult.Cancel;
-            btnCancel.Location = new Point(559, 202);
-            btnCancel.Name = "btnCancel";
-            btnCancel.Size = new Size(75, 23);
-            btnCancel.TabIndex = 1;///////////////////// 2;
-            btnCancel.Text = "Cancel";
-            btnCancel.UseVisualStyleBackColor = true;
-            btnCancel.Click += new EventHandler(btnOkCancel_Click);
-            Controls.Add(btnCancel);
-            #endregion
-            #endregion
-
-            #region Define form settings
-            AutoSize = true;
-            //ClientSize = new Size(646, 237);//gregt auto size this ?
-            Name = "UserInputForm";
-            ResumeLayout(false);
-            #endregion
+            DefineFormSettings();
 
             PerformLayout();
+        }
+
+        private void DefineFormSettings()
+        {
+            AutoSize = true;
+            ////////////////////////////////////////////////////////ClientSize = new Size(646, 237);
+            Name = "UserInputForm";
+            ResumeLayout(false);
+        }
+
+        private void AddFormControls(string solutionDirectory, string tempDirectory, string generatorName, string regularProjectName)
+        {
+            var btnCancel = new Button
+            {
+                DialogResult = DialogResult.Cancel,
+                Location = new Point(559, 202),//gregt auto-position ?
+                Name = "btnCancel",
+                Size = CommonButtonSize(),
+                TabIndex = 1,
+                Text = "Cancel",
+                UseVisualStyleBackColor = true
+            };
+
+            var btnOk = new Button
+            {
+                DialogResult = DialogResult.OK,
+                Location = new Point(464, 202),//gregt auto-position ?
+                Name = "btnOk",
+                Size = CommonButtonSize(),
+                TabIndex = 0,
+                Text = "OK",
+                UseVisualStyleBackColor = true
+            };
+
+            var label1 = new Label
+            {
+                AutoSize = true,
+                Location = new Point(25, 22),//gregt auto-position ?
+                Name = "label1",
+                ////////////////////////////////////////////////////////////////////////////////////////////////label1.Size = new Size(35, 13);
+                Text = DialogHelper.GetLabelText(solutionDirectory, tempDirectory, generatorName, regularProjectName)
+            };
+
+            // Button click events
+            btnCancel.Click += new EventHandler(btnOkCancel_Click);
+            btnOk.Click += new EventHandler(btnOkCancel_Click);
+
+            // Add controls to form
+            Controls.Add(btnCancel);
+            Controls.Add(btnOk);
+            Controls.Add(label1);
+        }
+
+        private Size CommonButtonSize()
+        {
+            return new Size(75, 23);
         }
 
         private void btnOkCancel_Click(object sender, EventArgs e)
