7965cafb62602d1954e79ff8a2db769cafc6ea85
Greg Trevellick <gtrevellick@googlemail.com>
2018-12-30 01:52:09 +0000

    .

--- Src/Common/DialogHelper.cs
+++ Src/Common/DialogHelper.cs
@@ -8,16 +8,16 @@ namespace Common
         {
             return 
                 $"The following will happen:" + Environment.NewLine +
+                $"{Environment.NewLine}" +
                 $" - A new project named '{regularProjectName}' will be created at {solutionDirectory}{Environment.NewLine}" +
                 $" - A command prompt window will open and run the following commands{Environment.NewLine}" +
                 $"    - npm install -g yo generator-{generatorName}{Environment.NewLine}" +
                 $"    - yo {generatorName}{Environment.NewLine}" +
                 $" - The new yeoman generated '{generatorName}' project will be launched in a NEW instance of Visual Studio{Environment.NewLine}" +
                 $" - The '{regularProjectName}' project (which is actually just an empty folder) will be moved{Environment.NewLine}" +
-                $" from{Environment.NewLine}" +
-                $"      {solutionDirectory}{Environment.NewLine}" +
-                $" to{Environment.NewLine}" +
-                $"      {tempDirectory}{Environment.NewLine}_gregtdatetimesuffix" +
+                $"        from    {solutionDirectory}{Environment.NewLine}" +
+                $"        to      {tempDirectory} (albeit with a unique date/time identifier suffix){Environment.NewLine} " +
+                $"{Environment.NewLine}" + 
                 $"{Environment.NewLine}Click OK to proceed.";
         }
     }
