27a30524b901f05782b5ee8f9d6509cf289e4388
Greg Trevellick <gtrevellick@googlemail.com>
2018-12-30 02:09:22 +0000

    .

--- Src/Common/DialogHelper.cs
+++ Src/Common/DialogHelper.cs
@@ -9,17 +9,25 @@ namespace Common
         public static string GetLabelText(string solutionDirectory, string tempDirectory, string generatorName, string regularProjectName)
         {
             return 
-                $"The following will happen:" + Environment.NewLine +
+                $"The following will happen:{Environment.NewLine}" +
                 $"{Environment.NewLine}" +
-                $" - A new project named '{regularProjectName}' will be created at {solutionDirectory}{Environment.NewLine}" +
+                $" - A new project called {regularProjectName} will be created at {solutionDirectory}{Environment.NewLine}" +
+                $"{Environment.NewLine}" +
                 $" - A command prompt window will open and run the following commands{Environment.NewLine}" +
-                $"    - npm install -g yo generator-{generatorName}{Environment.NewLine}" +
-                $"    - yo {generatorName}{Environment.NewLine}" +
-                $" - The new yeoman generated '{generatorName}' project will be launched in a NEW instance of Visual Studio{Environment.NewLine}" +
-                $" - The '{regularProjectName}' project (which is actually just an empty folder) will be moved{Environment.NewLine}" +
+                $"{Environment.NewLine}" +
+                $"      npm install -g yo generator-{generatorName}{Environment.NewLine}" +
+                $"{Environment.NewLine}" +
+                $"      yo {generatorName}{Environment.NewLine}" +
+                $"{Environment.NewLine}" +
+                $"   (this will scaffold your new project locally){Environment.NewLine}" +
+                $"{Environment.NewLine}" +
+                $" - The newly generated {generatorName} project will be launched in a new instance of Visual Studio{Environment.NewLine}" +
+                $"{Environment.NewLine}" +
+                $" - The {regularProjectName} project (nothing more than an empty folder) will be moved{Environment.NewLine}" +
+                $"{Environment.NewLine}" +
                 $"        from    {solutionDirectory}{Environment.NewLine}" +
-                $"        to      {tempDirectory} (albeit with a unique date/time identifier suffix){Environment.NewLine} " +
-                $"{Environment.NewLine}" + 
+                $"{Environment.NewLine}" +
+                $"        to       {tempDirectory} (with a unique date/time identifier suffix){Environment.NewLine} " +
                 $"{Environment.NewLine}Click {Ok} to proceed.";
         }
     }
