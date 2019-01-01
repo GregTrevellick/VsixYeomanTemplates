a8514e6d2477b6bd305d521257a05436a51fcab0
Greg Trevellick <gtrevellick@googlemail.com>
2019-01-01 19:39:22 +0000

    .

--- Src/CommonUi/WizardImplementation.cs
+++ Src/CommonUi/WizardImplementation.cs
@@ -5,7 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Windows;
 
-namespace AngularBasicVsix
+namespace VsixCommonWizard
 {
     public class WizardImplementation : IWizard 
     {
