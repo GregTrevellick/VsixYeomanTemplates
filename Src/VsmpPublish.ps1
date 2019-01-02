eef17cb8638d50d0afb5e5c7dadaf3bb1609923d
Greg Trevellick <gtrevellick@googlemail.com>
2019-01-02 21:06:12 +0000

    .

---
+++ Src/VsmpPublish.ps1
@@ -0,0 +1,5 @@
+# https://docs.microsoft.com/en-us/visualstudio/extensibility/walkthrough-publishing-a-visual-studio-extension-via-command-line?view=vs-2017
+
+cd C:\_git\OpenInApp.Launcher\src\packages\Microsoft.VSSDK.BuildTools.15.8.3252\tools\vssdk\bin
+
+.\VsixPublisher.exe publish  -payload "..\..\..\..\..\..\..\VsixYeomanTemplates\src\AngularBasicVsix\bin\debug\AngularBasicVsix.vsix"  -publishManifest "..\..\..\..\..\..\..\VsixYeomanTemplates\src\AngularBasicVsix\VsixArtefacts\VsmpPublish.json"  -personalAccessToken "vsmp_pat"
